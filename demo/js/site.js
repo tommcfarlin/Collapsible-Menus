89e50963d27cc649e01fb0ca8db0fc18a76328b3
Tom McFarlin <tom@tommcfarlin.com>
2012-04-11 22:38:23 -0400

    Continuing to build out the demo site

--- demo/js/site.js
+++ demo/js/site.js
@@ -1,13 +1,50 @@
 $(function() {
 
-	$('#menu').collapsible({
+	/* ----- Site Functions ----- */
+	
+	prettyPrint();
+	
+	$(document).scroll(function() {
+	
+		if( $(this).scrollTop() >= 133 ) {
+			
+			$('nav').addClass('subnav-fixed');
+			$('.hidden').removeClass('hidden');
+			
+		} else {
+		
+			if($('nav').hasClass('subnav-fixed')) {
+				
+				$('nav').removeClass('subnav-fixed');
+				$('.nav').children(':last')
+					.addClass('hidden');
+				
+			} // end if
+			
+		} // end if
+		
+	});
+	
+	$('button').click(function() {
+		window.location.href = 'https://github.com/tommcfarlin/Collapsible-Menus/zipball/master';
+	});
+
+	/* ----- Menu Demos----- */
+	
+	$('#vanilla-menu').collapsible({
 		effect: 'none',
 	    initialCollapse: true
+	});
+
+	$('#vanilla-menu li').each(function() {
+		if($(this).next().hasClass('expanded-menu') || $(this).next().hasClass('collapsed-menu')) {
+			$(this).addClass('has-children');
+		} // end if
 	});
 
 	$('#accordion').collapsible({
 		effect: 'slide',
 		initialCollapse: true
 	});
-	
+
 });
