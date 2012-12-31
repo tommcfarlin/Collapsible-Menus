2dd48bcc70420d0aff64c4098fccbf2f61df12a2
hightoweragency <jcooper@hightowerservices.com>
2012-12-31 09:20:16 -0800

    Close other open menus when parent is clicked
    Start Line 51: Added snippet to close other open menus when parent is clicked.

---
+++ src/jquery.collapsible-menus-update.js
@@ -0,0 +1,172 @@
+/*
+ * Collapsible Menus
+ * A jQuery plugin that makes it easy to create collapsible menus using nested, unordered lists.
+ *
+ * Copyright 2012 Tom McFarlin, http://tommcfarlin.com
+ * Released under the MIT License
+ * http://tommcfarlin.com/collapsible-menus/
+ *
+ * @version  1.1
+ */
+
+(function($) {
+	"use strict";
+
+	/*--------------------------------------------------*
+ 	 * Helper Functions
+	 *--------------------------------------------------*/
+	
+	/**
+	 * Initializes the menus by collapsing them (if specified) and attaching
+	 * event handlers controlling their display.
+	 *
+	 * opts   The plugins array of options
+	 */
+	function menuHandler(sId, opts) {
+
+		// Use the menu ID attribute to properly select nested menus
+		$('#' + sId + ' li ul').parent('li')
+			.prev()
+			.each(function() {
+			
+				// If set, collapse nested menus on load
+				if( opts.initialCollapse ) {
+					
+					$(this).next()
+						.addClass('collapsed-menu')
+						.hide();
+						
+				} else {
+				
+					$(this).next()
+						.addClass('expanded-menu');
+						
+				} // end if/else
+				
+				// Expand or collapse menu when the root menu is clicked
+				$(this).on('click', function() {
+					
+					var $menu = $(this).next();
+					var $menuHTML = $(this).children('a').html();
+					//Added Jcooper 12/28/2012
+					//Collapse all other open menus
+					$('#' + sId + ' li ul').parent('li').each( function() {
+						var $_menuHTML = $(this).prev('li').children('a').html();
+						if($_menuHTML != $menuHTML) { // this is not the one clicked, so close it... if open
+							if($(this).hasClass('expanded-menu')) {
+								switch(opts.effect.toString().toLowerCase()) {
+									case 'fade':
+										$(this).fadeOut('fast', function() {
+											$(this).removeClass('expanded-menu')
+												.addClass('collapsed-menu');
+										});
+									break;
+									
+									case 'slide':
+										$(this).slideUp('fast', function() {
+											$(this).removeClass('expanded-menu')
+												.addClass('collapsed-menu');
+										});
+									break;
+									
+									default:
+										$(this).removeClass('expanded-menu')
+											.addClass('collapsed-menu');
+									break;
+									
+								} // end switch/case	
+							}
+						}
+					});
+					
+					switch(opts.effect.toString().toLowerCase()) {
+					
+						case 'fade':
+						
+							if($menu.hasClass('expanded-menu' )) {
+							
+								$menu.fadeOut('fast', function() {
+									$(this).removeClass('expanded-menu')
+										.addClass('collapsed-menu');
+								});
+									
+							} else {
+								
+								$menu.removeClass('collapsed-menu')
+									.addClass('expanded-menu')
+									.fadeIn('fast');
+								
+							} // end if/else						
+						
+							break;
+							
+						case 'slide':
+						
+							if( $menu.hasClass('expanded-menu' ) ) {
+							
+								$menu.slideUp('fast', function() {
+									$(this).removeClass('expanded-menu')
+										.addClass('collapsed-menu');
+								});
+									
+							} else {
+								
+								$menu.removeClass('collapsed-menu')
+									.addClass('expanded-menu')
+									.slideDown('fast');
+								
+							} // end if/else
+						
+							break;
+							
+						default:
+							
+							if( $menu.hasClass('expanded-menu' ) ) {
+							
+								$menu.removeClass('expanded-menu')
+									.addClass('collapsed-menu')
+									.hide();
+									
+							} else {
+								
+								$menu.removeClass('collapsed-menu')
+									.addClass('expanded-menu')
+									.show();
+								
+							} // end if/else
+							
+							break;
+					
+					} // end switch/case
+
+				});
+				
+			});
+	
+	} // end menuHandler
+	
+	/*--------------------------------------------------*
+	* Default Settings
+	*--------------------------------------------------*/
+	
+	$.fn.collapsible = function(options) {
+	
+	    var opts = $.extend({}, $.fn.collapsible.defaults, options);
+		return this.each(function(evt) {
+
+			if( undefined === $(this).attr('id') ) {
+				throw "Specified element does not include an ID attribute.";
+			} else {
+				menuHandler($(this).attr('id'), opts);	
+			} // end if/else
+			
+	    });
+	
+	}; // end collapse
+	
+	$.fn.collapsible.defaults = {
+	    effect: 'none',
+	    initialCollapse: false
+	}; // end defaults
+	
+}(jQuery));
